707e985e9f1b72c9b7c31f275695d2dc61b340c8
Francisco Paredes <paredesfrancisco031@gmail.com>
2024-11-09 12:57:53 -0300

    sin productos en checkOut

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -3,6 +3,8 @@ import { useCart } from "../context/cartContext";
 import CheckoutForm from "./CheckoutForm";
 import { createOrder } from "../firebase/db";
 import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Checkout() {
   const { cart, clearCart, getTotal } = useCart();
@@ -30,13 +32,31 @@ function Checkout() {
         <h1 className="d-flex justify-content-center text-center mt-3">
           Tu orden fue creada con éxito! Número de orden: {orderId}
         </h1>
-        <Button className="btn btn-primary" as={Link} to="/">
-          Volver al Inicio
-        </Button>
+        <div  className="d-flex justify-content-center text-center mt-3">
+          <Button className="btn btn-primary" as={Link} to="/">
+            Volver al Inicio
+          </Button>
+        </div>
       </>
     );
   }
 
+  if(cart.length === 0){
+    return (
+      <>
+        <h1 className="d-flex justify-content-center text-center mt-3">
+          No tienes productos en el carrito
+        </h1>
+        <div  className="d-flex justify-content-center text-center mt-3">
+          <Button className="btn btn-primary" as={Link} to="/">
+            Volver al Inicio
+          </Button>
+        </div>
+
+      </>
+    );
+  }
+  
   return (
     <div className="p-5">
       <h1 className="d-flex justify-content-center">Checkout</h1>
